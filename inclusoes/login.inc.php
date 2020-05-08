ef5d0caf5c78783e493422c5a975e5a6f9f882d2
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-08 01:48:10 -0400

    script para validar login

---
+++ inclusoes/login.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+
+    if (isset($_POST['login-submit']))
+    {
+
+        require_once 'autoloader.inc.php';
+
+        $login = $_POST['login'];
+        $pwd = $_POST['pwd'];
+        
+        if (empty($login) || empty($pwd))
+        {
+
+            header("Location: ../login.php?error=emptyfields");
+            exit();
+
+        }
+        else
+        {
+
+            $user = new User();
+            $user->matricula = $login;
+            $user->username = $login;
+            $user->email = $login;
+            $user->pwd = $pwd;
+
+
+            if (!$user->auth())
+            {
+
+                header("Location: ../login.php?error=nouser");
+                exit();
+
+            }
+            else
+            {
+
+                session_start();
+                $_SESSION['username'] = $user->username;
+                $_SESSION['matricula'] = $user->matricula;
+                $_SESSION['perfil'] = $user->perfil;
+                
+                // var_dump($_SESSION['perfil']);
+
+                header("Location: ../admin.php");
+                exit();
+
+            }
+
+        }
+
+    }
+    else
+    {
+
+        header("Location: ../login.php");
+        exit();
+
+    }
