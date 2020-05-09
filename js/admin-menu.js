c04733424ab47cf6b6b651902bd2c311bb356dec
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-08 23:48:56 -0400

    Implementação do .js para a página admin
    - Implementações das funções específicas para a página de admin;

--- js/admin-menu.js
+++ js/admin-menu.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
     $('.navbar-burger').click(function(){
-        console.log('oi');
         $('.navbar-burger').toggleClass('is-active');
         $('.navbar-menu').toggleClass('is-active');
+    });
+});
 
+$(document).ready(function(){
+    $('.is-user-picture').click(function(){
+        $('.modal').toggleClass('is-active');
+    });
+});
+
+$(document).ready(function(){
+    $('.modal-background').click(function(){
+        $('.modal').toggleClass('is-active');
+    });
+});
+
+document.getElementById('user-choose-picture').addEventListener('click', function(){
+    document.getElementById('user-choose-picturev').click();
+})
+
+function button_fade_close()
+{
+
+    $('.delete').click(function(){
+        $('#notification').fadeOut('slow', 'swing');
     });
 
-});
+}
