a575083a694fce2ed257424de2c13aafa870164b
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-11 17:11:39 -0400

    Forma de atualização de foto de perfil atualizada
    - Devido à nova forma de fazer crop na imagem de perfil, a requisição enviada para o script php agora é diferente e a forma com que a imagem é armazenada no server precisou mudar apenas na ponta do controlador.

--- controllers/users.contr.php
+++ controllers/users.contr.php
@@ -1,11 +1,5 @@
 <?php
 
-    if (!isset($_POST['op']))
-    {
-        header("Location: ../admin.php");
-        exit();
-    }
-
     require '../inclusoes/autoloader.inc.php';
 
     function update_user_picture()
@@ -16,72 +10,19 @@
         $user->id = $_SESSION['id'];
 
         $user->fullUser();
+        $data = $_POST['image'];
+        $data = base64_decode(explode(',', explode(';', $data)[1])[1]);
 
-        $file = $_FILES['file'];
-        $fileName = $_FILES['file']['name'];
-        $fileTmpName = $_FILES['file']['tmp_name'];
-        $fileSize = $_FILES['file']['size'];
-        $fileError = $_FILES['file']['error'];
-        $fileType = $_FILES['file']['type'];
+        $imageName = '../users/profile_' . $user->username . '.png';
+        $user->perfil = 'users/profile_' . $user->username . '.png';
+        file_put_contents($imageName, $data);
 
-        $fileExt = explode('.', $fileName);
-        $fileActExt = strtolower(end($fileExt));
-
-        $allowed = array('jpg', 'jpeg', 'png');
-
-        if (in_array($fileActExt, $allowed))
-        {
-
-            if ($fileError === 0)
-            {   
-
-                if ($fileSize < 700000)
-                {
-
-                    $fileNewName = 'profile_' . $user->username . '.' . $fileActExt;
-                    $fileDest = '../users/' . $fileNewName;
-                    move_uploaded_file($fileTmpName, $fileDest);
-
-                    $user->perfil = 'users/' . $fileNewName;
-                    $user->updateFoto();
-                    $_SESSION['perfil'] = $user->perfil;
-
-                    header("Location: ../admin.php?upload=success");
-                    exit();
-
-                }
-                else
-                {
-
-                    header("Location: ../admin.php?error=imgtoolarge");
-                    exit();
-
-                }
-
-            }
-            else
-            {
-
-                header("Location: ../admin.php?error=upload");
-                exit();
-            }
-
-        }
-        else
-        {
-
-            header('Location: ../admin.php?error=extension');
-            exit();
-
-        }
+        $user->updateFoto();
+        $_SESSION['perfil'] = $user->perfil;
+        
+        echo $user->perfil;
 
     }
 
-    if ($_POST['op'] == 'updatephoto')
-    {
-
+    if ($_POST['image'])
         update_user_picture();
-        header("Location: ../admin.php");
-        exit();
-
-    }
