c6ddf26b011a01e026a9f5d7f6657c91c1852701
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-12 02:51:08 -0400

    Várias mudanças
    - Adicionado ícones de reconhecimento nos campos;
    - Adicionado verificação de usuário logado;
    - Inclusões necessárias para font awesome;

--- signup.php
+++ signup.php
@@ -1,3 +1,12 @@
+<?php session_start(); ?>
+
+<?php
+
+    if(isset($_SESSION['id']))
+        header("Location: admin.php");
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -29,38 +38,55 @@
 
     <section class="hero is-success is-fullheight">
         <div class="hero-body">
-            <div class="container has-text-centered">
+            <div class="container">
                 <div class="column is-4 is-offset-4">
 
                     <div class="box" >
                         <form method = 'post' action = 'inclusoes/signup.inc.php'>
                             <div class="field">
-                                <div class="control">
+                                <div id = 'div-matricula' class = "control has-icons-left">
                                     <input name = 'matricula' type="text" class="input is-medium" placeholder = 'Matrícula' maxlength = '4' required  autofocus />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-address-card"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div id = 'div-field-username' class = "field">
+                                <div id = 'div-username' class="control has-icons-left">
+                                    <input id = 'username_' name = 'username' type="text" class="input is-medium" minlength = '4' placeholder = 'Nome de usuário' autocomplete = 'off' required oninput = 'username_validation()' />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div id = 'div-field-email' class = "field">
+                                <div id = 'div-email' class="control has-icons-left">
+                                    <input id = 'email_' name = 'email' type="text" class="input is-medium" placeholder = 'E-mail' required oninput = 'email_validation()' />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-envelope"></i>
+                                    </span>
                                 </div>
                             </div>
                             <div class="field">
-                                <div class="control">
-                                    <input name = 'username' type="text" class="input is-medium" placeholder = 'Nome de usuário' required />
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input name = 'email' type="text" class="input is-medium" placeholder = 'E-mail' required />
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
+                                <div class="control has-icons-left">
                                     <input name = 'pwd' type="password" class="input is-medium" minlength = '6' placeholder = 'Insira uma senha' required />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-key"></i>
+                                    </span>
                                 </div>
                             </div>
                             <div class="field">
-                                <div class="control">
+                                <div class="control has-icons-left">
                                     <input name = 'rpwd' type="password" class="input is-medium" minlength = '6' placeholder = 'Repita a senha' required />
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-key"></i>
+                                        </span>
                                 </div>
                             </div>
                             <button type = 'submit' name = 'signup-submit' class="button is-fullwidth is-block is-success" >Cadastrar</button>
-                            <a href = 'login.php'><p class = 'help'>Já tem cadastro? Faça login</p></a>
+                            <div class = 'has-text-centered'>
+                                <a href = 'login.php'><p class = 'help'>Já tem cadastro? Faça login.</p></a>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -69,5 +95,10 @@
     </section>
 
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script type="text/javascript" src="js/signup-page.js"></script>
+<script src="plugins/fontawesome/js/fontawesome.min.js"></script>
+<script src="plugins/fontawesome/js/solid.min.js"></script>
+<script src="plugins/fontawesome/js/regular.min.js"></script>
 </html>
