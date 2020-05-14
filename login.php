df85450a60d00b4df4c8ea51a37cdd6abecd57f3
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-13 20:01:16 -0400

    Algumas mudanças
    - O novo método de validação usando AJAX demandou a mudança de alguns elementos da página;
    - Adição de mensagem de erro;
    - Correção no link da navbar

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
 
 <body>
 
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
 
             <div class="navbar-brand">
                 <a class="navbar-item" href="#">
@@ -29,7 +29,7 @@
 
             <div class="navbar-menu">
                 <div class="navbar-start">
-                    <a class="navbar-item" href = "singup.php">Cadastrar</a>
+                    <a class="navbar-item" href = "signup.php">Cadastrar</a>
                     <a class="navbar-item" href = "login.php">Login</a>
                 </div>
             </div>
@@ -42,29 +42,33 @@
                 <div class="column is-4 is-offset-4">
 
                     <div class="box" >
-                        <form method = 'post' action = 'inclusoes/login.inc.php'>
+                        
+                        <div id = 'notification-error' class="notification is-danger is-hidden">
+                            <button class="delete"></button>
+                            <div id = 'notification-error-content'></div>
+                        </div>
 
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input name = 'login' type="text" class="input is-large" placeholder = 'matrícula ou email ou username' required autofocus />
-                                    <span class="icon is-small is-left">
-                                        <i class="fas fa-user"></i>
-                                    </span>
-                                </div>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input id = 'login' type="text" class="input is-large" placeholder = 'matrícula ou email ou username' required autofocus />
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-user"></i>
+                                </span>
                             </div>
-                            
-                            <div class="field">
-                                <div class="control has-icons-left">
-                                    <input name = 'pwd' type="password" class="input is-large" minlength = '6' placeholder = 'Insira sua senha' required />
-                                    <span class="icon is-small is-left">
-                                        <i class="fas fa-key"></i>
-                                    </span>
-                                </div>
+                        </div>
+                        
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input id = 'pwd' type="password" class="input is-large" minlength = '6' placeholder = 'Insira sua senha' required />
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-key"></i>
+                                </span>
                             </div>
-                           
-                            <button type = 'submit' name = 'login-submit' class="button is-fullwidth is-block is-success" >Login</button>
-                            <a href = 'signup.php'><p class = 'help'>Não tem cadastro? Faça um.</p></a>
-                        </form>
+                        </div>
+                        
+                        <button id = 'submit-button' class="button is-fullwidth is-block is-success" >Login</button>
+                        <a href = 'signup.php'><p class = 'help'>Não tem cadastro? Faça um.</p></a>
+
                     </div>
                 </div>
             </div>
