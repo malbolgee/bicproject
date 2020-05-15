19d46d5f2211865d2be54862f55274de750ee89b
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-05-14 21:24:48 -0400

    Novas implementações
    - Implementação da função de controle para inserção de empregado no banco;

--- controllers/users.contr.php
+++ controllers/users.contr.php
@@ -2,6 +2,7 @@
 
     require_once '../class/dbh.class.php';
     require_once '../class/user.class.php';
+    require_once '../class/colab.class.php';
 
     function update_user_picture()
     {
@@ -25,5 +26,67 @@
 
     }
 
-    if ($_POST['image'])
+    function insert_employee()
+    {
+
+        $matricula =           $_POST['matricula'];
+        $nome =                $_POST['nome'];
+        $cpf =                 $_POST['cpf'];
+        $programacao =         $_POST['programacao'];
+        $inicio_periodo =      $_POST['inicio_periodo'];
+        $fim_periodo =         $_POST['fim_periodo'];
+        $direito_ferias =      $_POST['direito_ferias'];
+        $inicio_ferias =       $_POST['inicio_ferias'];
+        $fim_ferias =          $_POST['fim_ferias'];
+        $ferias =              $_POST['ferias'];
+        $pagamento =           $_POST['pagamento'];
+        $retorno_ferias =      $_POST['retorno_ferias'];
+
+        if (empty($matricula) || empty($nome) ||empty($cpf) ||empty($programacao) ||empty($inicio_periodo) ||empty($fim_periodo) ||empty($direito_ferias) ||empty($inicio_ferias) ||empty($fim_ferias) ||empty($ferias) ||empty($pagamento) ||empty($retorno_ferias))
+            return 'emptyfields';
+        if (!preg_match("/^[A-Z]+( [A-Z]+)*$/", $nome))
+            return 'invalidname';
+        else if (!preg_match("/\d{11}/", $cpf))
+            return 'cpfchar';
+        else if (strlen(($cpf) < 11 || strlen($cpf) > 11))
+            return 'cpflength';
+        else if ((int)$matricula < 0)
+            return 'negative';
+        else
+        {
+
+            if (Employee::get_employee($matricula) == true)
+                return 'employeeexists';
+            else
+            {
+
+                $employee = new Employee();
+
+                $employee->matricula =      $matricula;
+                $employee->nome =           $nome;
+                $employee->cpf =            $cpf;
+                $employee->programacao =    $programacao;
+                $employee->inicio_periodo = $inicio_periodo;
+                $employee->fim_periodo =    $fim_periodo;
+                $employee->direito_ferias = $direito_ferias;
+                $employee->inicio_ferias =  $inicio_ferias;
+                $employee->fim_ferias =     $fim_ferias;
+                $employee->ferias =         $ferias;
+                $employee->pagamento =      $pagamento;
+                $employee->retorno_ferias = $retorno_ferias;
+
+                $employee->insert_employee();
+
+                return 'success';
+
+            }
+
+        }
+
+    }
+
+    if (isset($_POST['image']))
         update_user_picture();
+    
+    if (isset($_POST['cadastro']))
+        echo json_encode(insert_employee());
