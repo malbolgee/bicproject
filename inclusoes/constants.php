068b87aa0577d28591d157341936009114f75cdf
Malbolge <victor.oliveiragomes@outlook.com>
2020-06-30 15:45:52 -0400

    Update constants.php

--- inclusoes/constants.php
+++ inclusoes/constants.php
@@ -4,10 +4,4 @@
     define("USUARIO", "root");
     define("SENHA", "");
     define("DB", "cadastros");
-
-    // define("HOST", "sql304.epizy.com");
-    // define("USUARIO", "epiz_25639737");
-    // define("SENHA", "uq4bLFhEoup");
-    // define("DB", "epiz_25639737_cadastros");
-
 ?>
